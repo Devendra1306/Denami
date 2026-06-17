7d597b35cb1daab5d99e4b5ca78d39d386b4fc46
Your Name <you@example.com>
2026-06-17 14:14:54 +0530

    feat: Add Firestore export to Firebase config

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -1,5 +1,6 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCNXQR-um1l_1vo8pgeP6u-FPhiKNvV3rM",
@@ -14,5 +15,6 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
+const db = getFirestore(app);
 
-export { app, auth, googleProvider };
+export { app, auth, googleProvider, db };
